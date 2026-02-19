e7aa095e6373130af4e518de9411a65ca4ba23bd
hkanojiyacode19-del <hkanojiyacode19@gmail.com>
2026-02-19 09:12:09 +0530

    first ripo

---
+++ patten1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n,i=0;
+    cin>>n;
+    
+    while (i<n){
+    int j=0;
+        while (j<n){
+            cout<<'*';
+            
+            j++;
+        }
+        cout<<'\n';
+
+        i++;
+    }
+    return 0;
+}
